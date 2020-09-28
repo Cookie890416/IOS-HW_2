3f7db2b36e8df0ea7e112a152b7a72ec691dd34a
gitHub <jack940197@gmail.com>
2020-09-28 19:45:36 +0800

    Initial Commit

---
+++ Homework/HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkApp.swift
+//  Homework
+//
+//  Created by User06 on 2020/9/28.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
